1461deb352ee44b318b4dd684bf9629e1260e3b5
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2014-06-11 15:05:24 +0200

    Generate ecdsa and dsa aswell (hopefully)

--- manifests/hostkey.pp
+++ manifests/hostkey.pp
@@ -4,7 +4,7 @@ class ssh::hostkey (
 
     if $manage_hostkey {
         # generate and store key on master
-        $rsa_priv = ssh_keygen({name => "ssh_host_rsa_${::fqdn}", dir => 'ssh/hostkeys'}) 
+        $rsa_priv = ssh_keygen({type => 'rsa', name => "ssh_host_rsa_${::fqdn}", dir => 'ssh/hostkeys'}) 
         $rsa_pub  = ssh_keygen({name => "ssh_host_rsa_${::fqdn}", dir => 'ssh/hostkeys', public => 'true'}) 
 
         file { '/etc/ssh/ssh_host_rsa_key':
@@ -12,6 +12,7 @@ class ssh::hostkey (
             group   => 'root',
             mode    => 0600,
             content => $rsa_priv,
+            notify  => Service[$ssh::service_name]
         }
         file { '/etc/ssh/ssh_host_rsa_key.pub':
             owner   => 'root',
@@ -19,6 +20,44 @@ class ssh::hostkey (
             mode    => 0644,
             content => "ssh-rsa $rsa_priv host_rsa_${::hostname}\n",
         }
+
+        # generate and store key on master
+        $dsa_priv = ssh_keygen({type => 'dsa', name => "ssh_host_dsa_${::fqdn}", dir => 'ssh/hostkeys'}) 
+        $dsa_pub  = ssh_keygen({name => "ssh_host_dsa_${::fqdn}", dir => 'ssh/hostkeys', public => 'true'}) 
+
+        file { '/etc/ssh/ssh_host_dsa_key':
+            owner   => 'root',
+            group   => 'root',
+            mode    => 0600,
+            content => $dsa_priv,
+            notify  => Service[$ssh::service_name]
+        }
+        file { '/etc/ssh/ssh_host_dsa_key.pub':
+            owner   => 'root',
+            group   => 'root',
+            mode    => 0644,
+            content => "ssh-dsa $dsa_priv host_dsa_${::hostname}\n",
+        }
+
+        # generate and store key on master
+        $ecdsa_priv = ssh_keygen({type => 'ecdsa', name => "ssh_host_ecdsa_${::fqdn}", dir => 'ssh/hostkeys'}) 
+        $ecdsa_pub  = ssh_keygen({name => "ssh_host_ecdsa_${::fqdn}", dir => 'ssh/hostkeys', public => 'true'}) 
+
+        file { '/etc/ssh/ssh_host_ecdsa_key':
+            owner   => 'root',
+            group   => 'root',
+            mode    => 0600,
+            content => $ecdsa_priv,
+            notify  => Service[$ssh::service_name]
+        }
+        file { '/etc/ssh/ssh_host_ecdsa_key.pub':
+            owner   => 'root',
+            group   => 'root',
+            mode    => 0644,
+            content => "ssh-ecdsa $ecdsa_priv host_ecdsa_${::hostname}\n",
+        }
+
+
     }
 }
 
